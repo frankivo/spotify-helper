d3da31239fe0e3cef8dd521347dc1a30339feb1f
Frank Oosterhuis <frank@datprof.com>
2018-04-03 11:10:15 +0200

    Add Pause

---
+++ Pause/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Pause
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var helper = new SpotifyHelper.SpotifyHelper();
+            helper.Pause();
+        }
+    }
+}
